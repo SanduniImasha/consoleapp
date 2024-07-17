6e761a6523f50b650b225ff81f54ca7b9ad36fb7
sanduni <sanduniimasha89@gmail.com>
2024-07-17 13:24:25 +1000

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+//using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ConsoleApp1;
+
+
+namespace JsonPractice;
+
+class Program
+{
+    
+    static async Task Main(string[] args)
+    {
+        string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+        HttpClient httpClient = new HttpClient();  //http client to get request  
+
+        try
+        {
+            var httpResponseMessage = await httpClient.GetAsync(url); //get request
+                                                                      //read the string from the response's content
+            string jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonResponse);
+
+
+            Result myPosts = JsonConvert.DeserializeObject<Result>(jsonResponse);
+
+            Console.WriteLine(JsonConvert.SerializeObject(myPosts.BPI.GBP));
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ ConsoleApp1/Result.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Result
+    {
+        public TimeDetail Time { get; set; }
+        public string disclaimer { get; set; }
+        public string chartName { get; set; }
+        public BPIDetails BPI { get; set; }
+
+        public class TimeDetail
+        {
+            public String updated { get; set; }
+            public String updatedISO { get; set; }
+            public String updateduk { get; set; }
+        }
+
+        public class Currency
+        {
+            public string Code { get; set; }
+            public string Symbol { get; set; }
+            public string Rate { get; set; }
+            public string Description { get; set; }
+            public double RateFloat { get; set; }
+        }
+
+        public class BPIDetails
+        {
+            public Currency USD { get; set; }
+            public Currency GBP { get; set; }
+            public Currency EUR { get; set; }
+        }
+
+
+    }
+}
